ee8708dc6ef975bcff224a45a18b2cdebb5e3237
Shintaro-Kamiya <95954535+Shintaro-Kamiya@users.noreply.github.com>
2022-06-11 16:23:53 +0900

    天気を拾うやつ

--- Hello_world.py
+++ Hello_world.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pandas as pd
-st.write("helloworld")
+import requests
+import json
+option = st.selectbox("地域選択",("静岡","東京"))
+region = {"静岡": "220000","東京": "130000"}
+id = region[option]
+url = f'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{id}.json'
+response = requests.get(url)
+d = response.json()
+t1 = "時間"+d["reportDatetime"]
+t2 = d["text"]
+st.write(t1)
+st.write(t2)
